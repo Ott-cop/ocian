0a809935df4cbca5a6c714bbe47368f2a6c4298c
Ott-cop <otavio.onoue@outlook.com>
2024-01-26 14:03:42 -0300

    Fix text system centering

--- src/components/System/SectionSystem/SectionSystem.tsx
+++ src/components/System/SectionSystem/SectionSystem.tsx
@@ -13,10 +13,13 @@ export default function SectionSystem() {
             <div className="w-full relative mt-[130px] justify-center">
                 <div className="block justify-center hiddenable">
                     <div className="flex relative justify-center w-full mb-6 ">
-                        <div className="text-width w-[1000px]">
+                        <div className="text-width w-[1000px] justify-center">
                             <h2 className="title-service text-[40px] font-medium text-center mb-2">NOSSO <span className="text-primary-color">SISTEMA</span></h2>
-                            <p className="paragraph-AboutUs text-[20px] text-left mb-14">
-                                        Os sistemas são desenvolvidos para seguir todas as normas aplicadas e também para que atenda as particularidades de cada serventia extrajudicial. Contudo, priorizamos entregar praticidade e alto desempenho, com rotinas simplificadas para otimizar o fluxo de serviço interno com dinamismo e segurança. Devido ao controle de acessos por usuários e registros de suas atividades, a parametrização e personalização de uso são agregadas; além disso, possuem <span className="text-primary-color">controle de caixa</span>, <span className="text-primary-color">controle financeiro</span>, <span className="text-primary-color">relatório de portal extrajudicial</span> e <span className="text-primary-color">CNJ</span> e <span className="text-primary-color">muito mais</span>.</p>
+                            <div className="flex justify-center">
+                                <p id="system-paragraph" className="paragraph-AboutUs text-[20px] text-left mb-14">
+                                            Os sistemas são desenvolvidos para seguir todas as normas aplicadas e também para que atenda as particularidades de cada serventia extrajudicial. Contudo, priorizamos entregar praticidade e alto desempenho, com rotinas simplificadas para otimizar o fluxo de serviço interno com dinamismo e segurança. Devido ao controle de acessos por usuários e registros de suas atividades, a parametrização e personalização de uso são agregadas; além disso, possuem <span className="text-primary-color">controle de caixa</span>, <span className="text-primary-color">controle financeiro</span>, <span className="text-primary-color">relatório de portal extrajudicial</span> e <span className="text-primary-color">CNJ</span> e <span className="text-primary-color">muito mais</span>.</p>
+                            </div>
+
                         </div>
                     </div>
                     <div className="flex w-full justify-center">
